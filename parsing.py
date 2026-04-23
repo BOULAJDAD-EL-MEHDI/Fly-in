161aaa109cd0de148b0968e46dba49796b00c161
El Mehdi Boulajdad <eboulajd@e2r7p1.1337.ma>
2026-04-23 16:17:59 +0100

    L

--- parsing.py
+++ parsing.py
@@ -1,21 +1,17 @@
-class Vertice:
-    def __init__(self, name):
-        self.name = name
-        self.neighbors = []
-
-def add_edge(connection):
-    name1, name2 = connection.split('-')
-    obj1 = Vertice(name1)
-    obj2 = Vertice(name2)
-    obj1.neighbors.append(obj2)
-    obj2.neighbors.append(obj1)
-
-    for n in obj1.neighbors:
-        print(n.name)
-    for n in obj2.neighbors:
-        print(n.name)
-    
+def manual_parser():
+    f = open("config.txt", "r")
+    text = f.readlines
+    return text
 
 
-connection = 'start-juntion'
-add_edge(connection)
+def parser():
+    try:
+        txt = manual_parser()
+        return txt
+    except Exception as e:
+        print(e)
+
+
+if __name__ == "__main__":
+    text = parser()
+    print(text)
